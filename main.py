22e82b01187f9bf3ddeee7d66841f746e6a97841
Aadish Saini <63640902+AadishSaini@users.noreply.github.com>
2021-02-16 11:51:52 +0530

    intial files commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# from selenium import webdriver
+# import time
+
+# browser = webdriver.Chrome('./chromedriver.exe')
+
+#browser.get("https://youtube.com")
+
+#time.sleep(10)
+#browser.close()
+
+
+import pygame
