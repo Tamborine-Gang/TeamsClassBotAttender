4a5b84ddccb57187510ec70b58b62e7f4d22c5d7
AadishSaini <sainiaadish8@gmail.com>
2021-02-19 12:30:26 +0530

    text messages while running

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def load_site():
 
 def login(username, password):
 	# username 
+	print("[Succesful] :: Successfully loaded website ...")
+	print("[Process] :: Now logging in")
 	input_field = browser.find_element_by_xpath('/html/body/div/form[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/input[1]')
 	input_field.send_keys(username)
 	next_button = browser.find_element_by_xpath("/html/body/div/form[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div/div/div[4]/div/div/div/div/input")
@@ -32,15 +34,18 @@ def login(username, password):
 	no_but.click()
 
 	time.sleep(15)
+	print('[Succesful] :: Successfully logged in...')
 
 	teams_mode_but = browser.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/app-bar/nav/ul/li[2]/button')
 	teams_mode_but.click()
 	
+
 # credentials loading
 with open('./username.txt', 'r') as f:
 	username = f.readlines()
 with open('./password.txt', 'r') as f:
 	password = f.readlines()
+
 
 # operating system detection for chromdriver
 if "linux" in platform.platform().lower():
@@ -48,8 +53,10 @@ if "linux" in platform.platform().lower():
 else:
 	PATH = "./chromedriver.exe"
 
+print("[Process] :: Loading driver")
 # init the driver
 browser = webdriver.Chrome(PATH)
+print("[Succesful] :: loaded the driver")
 
 load_site()
 time.sleep(3)
